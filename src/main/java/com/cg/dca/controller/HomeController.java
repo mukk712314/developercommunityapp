2c935dadc9805c44548a249e3277c3617b8e9c6a
mukk712314 <mukku12314@gmail.com>
2022-08-10 13:36:22 +0530

    login page

---
+++ src/main/java/com/cg/dca/controller/HomeController.java
@@ -0,0 +1,21 @@
+package com.cg.dca.controller;
+
+
+	import org.springframework.web.bind.annotation.GetMapping;
+	import org.springframework.web.bind.annotation.RestController;
+	
+	@RestController
+	public class HomeController {
+
+	    @GetMapping("/home")
+	    public String home(){
+	        return "This is Home Page";
+	    }
+
+	    @GetMapping("/admin")
+	    public String admin(){
+	        return "This is Admin Page";
+	    }
+
+	}
+	
---
+++ src/main/java/com/cg/dca/entity/User.java
@@ -0,0 +1,58 @@
+package com.cg.dca.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="user")
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long userId;
+	private String username;
+	private String password;
+	private String role;
+	
+	public Long getUserId() {
+		return userId;
+	}
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+}
+	/*@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		User user = (User) o;
+		return userId.equals(user.userId);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(userId);
+	}*/
+
+	
